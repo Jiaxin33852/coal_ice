174c5d7a4e282df40e03506af1c777fa9ecbb6e8
Jiaxin33852 <117554970+chenziqian02@users.noreply.github.com>
2024-04-23 03:12:00 +0800

    create

---
+++ scripts/indexScript.js
@@ -0,0 +1,25 @@
+// 在 indexScript.js 文件中
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.getElementById('myRange');
+    const container = document.querySelector('.container');
+
+    slider.addEventListener('input', function() {
+        let contentFile = '/entries/index-content.html'; // 默认加载首页内容
+
+        if (this.value == 1) {
+            contentFile = '/entries/index-content.html'; // 首页内容文件
+        } else if (this.value == 2){
+            contentFile = '/entries/about-content.html';
+        }
+
+        // 使用fetch API来加载HTML文件内容
+        fetch(contentFile)
+            .then(response => response.text())
+            .then(html => {
+                container.innerHTML = html;
+            })
+            .catch(error => {
+                console.error('Failed to load content:', error);
+            });
+    });
+});
